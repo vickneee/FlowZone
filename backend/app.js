a7cf43e2070fa20ffe28ae3675fe398a9810e88f
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-05-09 19:47:31 +0300

    Initialize server with user authentication and routing

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const userRouter = require("./routes/userRouter");
+const { unknownEndpoint,errorHandler } = require("./middleware/customMiddleware");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+// Middlewares
+app.use(cors())
+app.use(express.json());
+
+connectDB().catch((err) => {
+  console.error("Database connection failed:", err);
+});
+
+// Serve the static files from the React app (frontend) in the dist folder
+app.use(express.static('view'))
+
+// Use the userRouter for all "/jobs" routes
+app.use("/api/users", userRouter);
+
+// Path
+app.get((req, res) => {
+  res.sendFile(__dirname + '/view/index.html');
+});
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+module.exports = app;
---
+++ backend/models/userModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    lastLogin: { type: Date, default: Date.now },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+  // Add virtual field id
+  userSchema.set('toJSON', {
+    virtuals: true,
+    transform: (doc, ret) => {
+      ret.id = ret._id;
+      return ret;
+    }
+  });
+
+module.exports = mongoose.model("User", userSchema);
